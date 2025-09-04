9ca1a664b79cd1a9264793d3c143877cdef240fb
adriantae <139446721+prdiptaa@users.noreply.github.com>
2025-09-04 14:33:05 +0800

    update porto

--- dist/js/script.js
+++ dist/js/script.js
@@ -19,3 +19,29 @@ hamburger.addEventListener('click', function () {
     hamburger.classList.toggle('hamburger-active');
     navMenu.classList.toggle('hidden');
   });
+
+//changeText
+const texts = ["Frontend Developer", "Web Developer "];
+let textIndex = 0;
+let charIndex = 0;
+let isDeleting = false;
+
+(function type() {
+  const element = document.getElementById("changeText");
+  const fullText = texts[textIndex];
+
+  element.textContent = isDeleting
+    ? fullText.substring(0, charIndex--)
+    : fullText.substring(0, charIndex++);
+
+  if (!isDeleting && charIndex === fullText.length) {
+    isDeleting = true;
+    setTimeout(type, 2000); // jeda sebelum menghapus teks
+  } else if (isDeleting && charIndex === 0) {
+    isDeleting = false;
+    textIndex = (textIndex + 1) % texts.length;
+    setTimeout(type, 500); // jeda sebelum mengetik teks berikutnya
+  } else {
+    setTimeout(type, isDeleting ? 100 : 200);
+  }
+})();
